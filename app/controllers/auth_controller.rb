116fc8f69598c4b26036f66dfc1ad02ab1577fb2
Lauren Go <lauren.nicole.go@gmail.com>
2015-05-06 18:26:21 -0700

    auth and oauth for both user types

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,5 +1,39 @@
 class AuthController < ApplicationController
 
-    # routes
+    def failure
+      flash[:danger] = "FACEBOOK LOGIN ERROR. Please try again!"
+      redirect_to login_path
+    end
 
+    def callback
+      provider_user = request.env['omniauth.auth']
+
+      user = User.find_or_initialize_by(provider_id:provider_user['uid'], provider: params[:provider])
+
+      if user.new_record?
+        user.password_digest = "fb_authenticated"
+        user.provider_hash = provider_user['credentials']['token']
+        user.name = provider_user['info']['name']
+        user.email = provider_user['info']['email']
+        user.gender = provider_user['extra']['raw_info']['gender']
+        user.image = provider_user['info']['image']
+        user.status = true
+        user.user_type = "consumer"
+        if user.save
+          flash[:success] = "Your account has successfully been created!"
+          session[:user_id] = user.id
+          #CHANGE TO REDIRECT AND AUTO LOGIN
+          redirect_to root_path
+        else
+          flash[:danger] = "Error. Invalid information inputed."
+          redirect_to new_user_path
+        end
+
+      else
+        flash[:success] = "You have been successfully logged in!"
+        session[:user_id] = user.id
+        redirect_to root_path
+        # render :json => provider_user
+      end
+    end
 end
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,10 @@
 class SiteController < ApplicationController
 
-    # routes
+    def index
+
+    end
+
+
+
 
 end
