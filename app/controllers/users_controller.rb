ab161963455d0a95a08e77b0f019955f69aa0a45
piket <timonpike@gmail.com>
2015-05-06 18:29:04 -0700

    Added profile pages with editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
 
-    # routes
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        user_info = user_params_extended
+        user = User.find session[:user_id]
+        for field in user_info do
+            user[field] = user_info[field]
+        end
+        user.save
+        redirect_to user
+    end
+
+    private
+
+    def user_params_extended
+        params.require(:user).permit(:email, :name, :gender, :location, :age, :image)
+    end
 
 end
