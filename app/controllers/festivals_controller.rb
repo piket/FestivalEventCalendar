c5562439e4ce7a463bc3ed05ff2ddf5e31600677
Lauren Go <lauren.nicole.go@gmail.com>
2015-05-07 20:15:56 -0700

    fixed dropdown

--- app/controllers/festivals_controller.rb
+++ app/controllers/festivals_controller.rb
@@ -1,15 +1,16 @@
 class FestivalsController < ApplicationController
 
     def index
+      #Find all festivals (users that are both type host and verified)
       festivals = User.where(user_type:'host',status:true)
-        # render :json => festivals
+        # Loop through festivals and for each grab the events that they are hosting and grab tags associated with those events.
         @festival_details = festivals.map do |f|
          tags = f.hosted_events.map do |event|
                    event.tags
                end
-
+        # Create an array of tags in strings and put dashes in between tags with multiple words. Also filter out multiple occurrences of tags.
          tags = tags.flatten.reduce([]) {|arr,tag|arr << tag.name.gsub(" ", "-") unless arr.include? tag.name}
-
+         # Create a hash of pertinent data.
           temp_hash = {
             name: f.name,
             location: f.location,
