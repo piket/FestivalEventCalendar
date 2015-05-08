cd17cfba499d988c3d48dc47f69d26cdf409839b
piket <timonpike@gmail.com>
2015-05-08 10:47:11 -0700

    Can noew upload an excel or csv file to create events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -47,6 +47,56 @@ class EventsController < ApplicationController
    def destroy
    end
 
+   def import
+     rows = Event.import(params[:file])
+     # render json: rows
+     # return
+     rows.each do |row|
+         event = Event.create({name:row['name'], description: row['description'], image: row['image'], video: row['video'], link: row['link'], purchase: row['purchase'], price: row['price']})
+
+         if row['dates'].length == row['times'].length && row['dates'].length == row['locations'].length
+            for x in 0...row['dates'].length
+               row['times'][x][0] = row['times'][x][0].to_i
+               row['times'][x][0] += 12 if row['times'][x][2].casecmp('pm') == 0 && row['times'][x][0] != 12
+               datetime = DateTime.new(row['dates'][x][2].to_i,row['dates'][x][0].to_i,row['dates'][x][1].to_i,row['times'][x][0],row['times'][x][1].to_i)
+               event.event_occurrences.create({date: datetime, location: row['locations'][x]})
+            end
+         elsif row['dates'].length >= row['times'].length && row['dates'].length >= row['locations'].length
+            for x in 0...row['dates'].length
+               row['times'][0][0] = row['times'][0][0].to_i
+               row['times'][0][0] += 12 if row['times'][0][2].casecmp('pm') == 0 && row['times'][0][0] != 12
+               datetime = DateTime.new(row['dates'][x][2].to_i,row['dates'][x][0].to_i,row['dates'][x][1].to_i,row['times'][0][0],row['times'][0][1].to_i)
+               event.event_occurrences.create({date: datetime, location: row['locations'][0]})
+               # render json: {row:row,occurrences:event.event_occurrences}
+               # return
+            end
+         elsif row['times'].length >= row['dates'].length && row['times'].length >= row['locations'].length
+            for x in 0...row['times'].length
+               row['times'][x][0] = row['times'][x][0].to_i
+               row['times'][x][0] += 12 if row['times'][x][2].casecmp('pm') == 0 && row['times'][x][0] != 12
+               datetime = DateTime.new(row['dates'][0][2].to_i,row['dates'][0][0].to_i,row['dates'][0][1].to_i,row['times'][x][0],row['times'][x][1].to_i)
+               event.event_occurrences.create({date: datetime, location: row['locations'][0]})
+            end
+         elsif row['locations'].length >= row['times'].length && row['locations'].length >= row['dates'].length
+            for x in 0...row['locations'].length
+               row['times'][0][0] = row['times'][0][0].to_i
+               row['times'][0][0] += 12 if row['times'][0][2].casecmp('pm') == 0 && row['times'][0][0] != 12
+               datetime = DateTime.new(row['dates'][0][2].to_i,row['dates'][0][0].to_i,row['dates'][0][1].to_i,row['times'][0][0],row['times'][0][1].to_i)
+               event.event_occurrences.create({date: datetime, location: row['locations'][x]})
+            end
+         end
+
+         row['tags'].each do |tag|
+            event.tags << Tag.find_or_create_by(name: tag)
+         end
+
+         @current_user.hosted_events << event
+      end
+      # render json: @current_user.hosted_events
+     flash[:success] = "File imported and new events added"
+     redirect_to root_path
+   end
+
 
    private
 
